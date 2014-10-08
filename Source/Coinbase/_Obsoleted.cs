6f151f0ffddd9e608c15bcdbe861f9b2bedb9214
bchavez <bchavez@bitarmory.com>
2014-10-08 16:55:52 -0700

    * Added [Obsolete] attributes with compiler error on moved classes * Some comment cleanup

---
+++ Source/Coinbase/_Obsoleted.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Coinbase
+{
+    [Obsolete("This class has been moved to Coinbase.Mvc assembly for MVC projects. You can get Coinbase.Mvc on NuGet.", true)]
+    public class CoinbaseCallback
+    {
+    }
+}
+namespace Coinbase.Serialization
+{
+    [Obsolete("This class has been moved to Coinbase.Mvc assembly for MVC projects. You can get Coinbase.Mvc on NuGet.", true)]
+    public class JsonNetBinderAttribute
+    {
+    }   
+}
