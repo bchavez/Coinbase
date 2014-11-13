634bcf8fb4c62be1d20bda90d2b0c66f9a9de692
bchavez <bchavez@bitarmory.com>
2014-11-13 12:08:47 -0800

    * Button Subscription Repeat validation test

--- Source/Coinbase.Tests/ValidationTests.cs
+++ Source/Coinbase.Tests/ValidationTests.cs
@@ -1,42 +1,43 @@
-﻿using NUnit.Framework;
+﻿using Coinbase.ObjectModel;
+using FluentValidation;
+using FluentValidation.Attributes;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
 
 namespace Coinbase.Tests
 {
-
     [TestFixture]
-    public class ValidationTests
+    public class AbstractValidationTest<T>
     {
-        [TestFixtureSetUp]
-        public void BeforeRunningTestSession()
-        {
+        private IValidatorFactory factory = new AttributedValidatorFactory();
 
-        }
-
-        [TestFixtureTearDown]
-        public void AfterRunningTestSession()
-        {
-
-        }
-
+        public IValidator<T> Validator { get; set; }
 
         [SetUp]
         public void BeforeEachTest()
         {
-
+            this.Validator = factory.GetValidator<T>();
         }
+    }
 
-        [TearDown]
-        public void AfterEachTest()
-        {
-
-        }
-
-
+    public class ButtonValidatorTests : AbstractValidationTest<ButtonRequest>
+    {
         [Test]
-        public void Test()
+        public void subscription_should_have_a_repeat_value()
         {
-            
+            var request = new ButtonRequest
+                {
+                    Name = "test", Price = 79.99m, Currency = Currency.USD
+                };
 
+            request.Subscription = false;
+            this.Validator.ShouldNotHaveValidationErrorFor(br => br.Repeat, request);
+
+            request.Subscription = true;
+            this.Validator.ShouldHaveValidationErrorFor(br => br.Repeat, request);
+
+            request.Repeat = SubscriptionType.Monthly;
+            this.Validator.ShouldNotHaveValidationErrorFor(br => br.Repeat, request);
         }
     }
 
