2e2445cf50fe6bf964da78d3d4d54e1da6e814f8
bchavez <bchavez@bitarmory.com>
2015-07-09 15:02:05 -0700

    * nuget push v0.3.12

--- Source/Coinbase/Properties/AssemblyInfo.cs
+++ Source/Coinbase/Properties/AssemblyInfo.cs
@@ -1,14 +1,12 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitleAttribute("Coinbase API for .NET")]
 [assembly: AssemblyProductAttribute("Coinbase API")]
 [assembly: AssemblyCompanyAttribute("Brian Chavez")]
 [assembly: AssemblyCopyrightAttribute("Brian Chavez © 2015")]
-[assembly: AssemblyVersionAttribute("0.3.11")]
-[assembly: AssemblyFileVersion("0.3.11")]
-[assembly: AssemblyInformationalVersion("0.3.11 built on 6/17/2015 4:36:14 PM UTC")]
+[assembly: AssemblyVersionAttribute("0.3.12")]
+[assembly: AssemblyFileVersion("0.3.12")]
+[assembly: AssemblyInformationalVersion("0.3.12 built on 7/9/2015 10:00:43 PM UTC")]
 [assembly: AssemblyTrademark("MIT License")]
 [assembly: AssemblyDescriptionAttribute("http://www.github.com/bchavez/Coinbase")]
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("Coinbase.Tests")]
--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-source\BuildFiles\fb.exe source\BuildFiles\BuildFiles.dll -c:BuildTask -p:Version="0.3.11"
+source\BuildFiles\fb.exe source\BuildFiles\BuildFiles.dll -c:BuildTask -p:Version="0.3.12"
