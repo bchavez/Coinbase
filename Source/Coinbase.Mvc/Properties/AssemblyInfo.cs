0f25e1947d1911e03b4163c2683072905f96c75d
bchavez <bchavez@bitarmory.com>
2015-09-11 11:47:04 -0700

    nuget push, fixes #19

--- Source/Coinbase.Mvc/Properties/AssemblyInfo.cs
+++ Source/Coinbase.Mvc/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyrightAttribute("Brian Chavez © 2015")]
 [assembly: AssemblyVersionAttribute("0.3.14")]
 [assembly: AssemblyFileVersion("0.3.14")]
-[assembly: AssemblyInformationalVersion("0.3.14 built on 9/11/2015 6:04:55 PM UTC")]
+[assembly: AssemblyInformationalVersion("0.3.14 built on 9/11/2015 6:44:52 PM UTC")]
 [assembly: AssemblyTrademark("MIT License")]
 [assembly: AssemblyDescriptionAttribute("http://www.github.com/bchavez/Coinbase")]
 [assembly: InternalsVisibleTo("Coinbase.Tests")]
