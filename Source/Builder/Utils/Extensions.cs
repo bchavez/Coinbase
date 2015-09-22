c36806555621f2d80a58e988c470ffab5b45b294
bchavez <bchavez@bitarmory.com>
2015-09-22 10:09:45 -0700

    build ci update.

--- Source/Builder/Utils/Extensions.cs
+++ Source/Builder/Utils/Extensions.cs
@@ -48,7 +48,7 @@ namespace Builder.Utils
                 return ver;
             ver = Environment.GetEnvironmentVariable("APPVEYOR_BUILD_VERSION")?.Trim();
             if (!string.IsNullOrWhiteSpace(ver))
-                return $"{ver}-ci";
+                return $"0.0.{ver}-ci";
             return "0.0.0-localbuild";
         }
     }
